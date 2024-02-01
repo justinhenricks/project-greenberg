f54094558854819c68009b787ec87bb7d7f00c7a
Justin Henricks <justinhenricks@gmail.com>
2024-01-31 21:44:24 -0500

    Write the function
    Check if customer VIP and is first order, if so find the express options, discount them 50%

--- extensions/vip-express-discount/src/run.ts
+++ extensions/vip-express-discount/src/run.ts
@@ -1,10 +1,4 @@
-import type {
-  RunInput,
-  FunctionRunResult
-} from "../generated/api";
-import {
-  DiscountApplicationStrategy,
-} from "../generated/api";
+import type { FunctionRunResult, RunInput } from "../generated/api";
 
 const EMPTY_DISCOUNT: FunctionRunResult = {
   discounts: [],
@@ -12,9 +6,56 @@ const EMPTY_DISCOUNT: FunctionRunResult = {
 
 type Configuration = {};
 
+interface DiscountTarget {
+  deliveryOption: {
+    handle: string;
+  };
+}
+
 export function run(input: RunInput): FunctionRunResult {
   const configuration: Configuration = JSON.parse(
     input?.discountNode?.metafield?.value ?? "{}"
   );
+
+  const customer = input?.cart?.buyerIdentity?.customer;
+  const deliveryGroups = input.cart.deliveryGroups;
+
+  if (
+    customer?.numberOfOrders === 0 &&
+    customer.hasTags.some((tag) => tag.hasTag)
+  ) {
+    let expressOptions: DiscountTarget[] = [];
+
+    // Iterate over each delivery group and find Express options
+    deliveryGroups.forEach((group) => {
+      group.deliveryOptions.forEach((option) => {
+        if (option.title === "Express") {
+          expressOptions.push({
+            deliveryOption: {
+              handle: option.handle,
+            },
+          });
+        }
+      });
+    });
+
+    // If any Express options were found, return discount for them
+    if (expressOptions.length > 0) {
+      return {
+        discounts: [
+          {
+            value: {
+              percentage: {
+                value: "50", // 50% discount
+              },
+            },
+            targets: expressOptions,
+            message: "50% off express shipping for our VIPs!",
+          },
+        ],
+      };
+    }
+  }
+
   return EMPTY_DISCOUNT;
-};
+}

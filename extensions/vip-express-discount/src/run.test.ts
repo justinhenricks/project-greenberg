827625959270593c59a5c08c197c13c29bedc4fe
Justin Henricks <justinhenricks@gmail.com>
2024-01-31 21:18:25 -0500

    wrote tests for no discount conditions

--- extensions/vip-express-discount/src/run.test.ts
+++ extensions/vip-express-discount/src/run.test.ts
@@ -66,4 +66,100 @@ describe("VIP express discount", () => {
 
     expect(result).toEqual(expected);
   });
+
+  it("returns no discount if not tagged VIP", () => {
+    const result = run({
+      discountNode: {
+        metafield: null,
+      },
+      cart: {
+        buyerIdentity: {
+          customer: {
+            numberOfOrders: 0,
+            hasTags: [
+              {
+                hasTag: false,
+                tag: "VIP",
+              },
+            ],
+          },
+        },
+        deliveryGroups: [
+          {
+            deliveryOptions: [
+              {
+                title: "Economy",
+                handle:
+                  "538e36d0567ff7ccdda95a73e62478cb-9e912b3dae9b0e04b6f1facf08aa7cb2",
+              },
+              {
+                title: "Standard",
+                handle:
+                  "538e36d0567ff7ccdda95a73e62478cb-ee768830e386b87e4f230f4292c237a3",
+              },
+              {
+                title: "Express",
+                handle:
+                  "538e36d0567ff7ccdda95a73e62478cb-9bc19cc1ae6807304d5d933ef3f1056d",
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    const expected: FunctionResult = {
+      discounts: [],
+    };
+
+    expect(result).toEqual(expected);
+  });
+
+  it("returns no discount if tagged VIP and not first order", () => {
+    const result = run({
+      discountNode: {
+        metafield: null,
+      },
+      cart: {
+        buyerIdentity: {
+          customer: {
+            numberOfOrders: 2,
+            hasTags: [
+              {
+                hasTag: true,
+                tag: "VIP",
+              },
+            ],
+          },
+        },
+        deliveryGroups: [
+          {
+            deliveryOptions: [
+              {
+                title: "Economy",
+                handle:
+                  "538e36d0567ff7ccdda95a73e62478cb-9e912b3dae9b0e04b6f1facf08aa7cb2",
+              },
+              {
+                title: "Standard",
+                handle:
+                  "538e36d0567ff7ccdda95a73e62478cb-ee768830e386b87e4f230f4292c237a3",
+              },
+              {
+                title: "Express",
+                handle:
+                  "538e36d0567ff7ccdda95a73e62478cb-9bc19cc1ae6807304d5d933ef3f1056d",
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    const expected: FunctionResult = {
+      discounts: [],
+    };
+
+    expect(result).toEqual(expected);
+  });
 });

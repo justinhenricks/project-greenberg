482750c8447526c94e357ee7825e01560026b6a0
Justin Henricks <justinhenricks@gmail.com>
2024-01-31 21:14:29 -0500

    write first failing test

--- extensions/vip-express-discount/src/run.test.ts
+++ extensions/vip-express-discount/src/run.test.ts
@@ -1,16 +1,67 @@
-import { describe, it, expect } from 'vitest';
-import { run } from './run';
-import { FunctionResult, DiscountApplicationStrategy } from '../generated/api';
+import { describe, expect, it } from "vitest";
+import { FunctionResult } from "../generated/api";
+import { run } from "./run";
 
-describe('shipping discounts function', () => {
-  it('returns no discounts without configuration', () => {
+describe("VIP express discount", () => {
+  it("returns 50% off express if tagged VIP and first order", () => {
     const result = run({
       discountNode: {
-        metafield: null
-      }
+        metafield: null,
+      },
+      cart: {
+        buyerIdentity: {
+          customer: {
+            numberOfOrders: 0,
+            hasTags: [
+              {
+                hasTag: true,
+                tag: "VIP",
+              },
+            ],
+          },
+        },
+        deliveryGroups: [
+          {
+            deliveryOptions: [
+              {
+                title: "Economy",
+                handle:
+                  "538e36d0567ff7ccdda95a73e62478cb-9e912b3dae9b0e04b6f1facf08aa7cb2",
+              },
+              {
+                title: "Standard",
+                handle:
+                  "538e36d0567ff7ccdda95a73e62478cb-ee768830e386b87e4f230f4292c237a3",
+              },
+              {
+                title: "Express",
+                handle:
+                  "538e36d0567ff7ccdda95a73e62478cb-9bc19cc1ae6807304d5d933ef3f1056d",
+              },
+            ],
+          },
+        ],
+      },
     });
+
     const expected: FunctionResult = {
-      discounts: [],
+      discounts: [
+        {
+          value: {
+            percentage: {
+              value: "100",
+            },
+          },
+          targets: [
+            {
+              deliveryOption: {
+                handle: "express",
+              },
+            },
+          ],
+          message: "50% off express shipping!",
+        },
+      ],
     };
 
     expect(result).toEqual(expected);
